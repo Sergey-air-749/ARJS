a0aaa99690b29f31d983d87298b110ea805e2658
Sergey-air-749 <michinssergeevich@gmail.com>
2025-01-01 20:54:24 +0500

    first commit

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let video2 = document.getElementById('video2');
 let startbutton = document.getElementById('startbutton');
 let stream = 0;
 
-navigator.mediaDevices.getUserMedia({ video: { width: 720, height: 720 }, audio: false })
+navigator.mediaDevices.getUserMedia({ video: { width: 720, height: 720, facingMode: "environment"}, audio: false })
 .then(function(stream) {
 
     video2.srcObject = stream;
